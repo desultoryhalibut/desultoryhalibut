bc11504ba21d530d6d08b774038dab8439b15ec6
Christine Chou <christinejchou@gmail.com>
2016-07-24 15:14:25 -0700

    Update code for seed data

--- server/workers/workers.js
+++ server/workers/workers.js
@@ -20,16 +20,16 @@ const KEYWORDS = ['car', 'real estate agent', 'inflation', 'restaurant', 'unempl
  * @param  {[Array]} trendsData [description]
  * @return {[Array]}            [description]
  */
-function reformatTrendsData(trendsData) {
-   return trendsData.map(item => {
-     for (var key in item) {
-       let obj = {};
-       obj['date'] = key;
-       obj['volume'] = item[key];
-       return obj;
-     }
-   });
-}
+// function reformatTrendsData(trendsData) {
+//    return trendsData.map(item => {
+//      for (var key in item) {
+//        let obj = {};
+//        obj['date'] = key;
+//        obj['volume'] = item[key];
+//        return obj;
+//      }
+//    });
+// }
 
  /**
   * function QueryGoogleTrends - Helper function to seed data from GoogleTrends API to database
@@ -37,129 +37,43 @@ function reformatTrendsData(trendsData) {
   * @param {[Array]} googleTrendData [return data from googleTrends search]
   */
 
-function queryGoogleTrends(key, googleTrendResults) {
-  let reformatData = reformatTrendsData(googleTrendResults);
+// function queryGoogleTrends(key, googleTrendResults) {
+//   let reformatData = reformatTrendsData(googleTrendResults);
 
-  let newData = {
-    'keyword': key,
-    'searchVolume': reformatData
-  };
+//   let newData = {
+//     'keyword': key,
+//     'searchVolume': reformatData
+//   };
 
-  GoogleTrends.find({'keyword': key})
-    .then(function(found) {
-      if (found.length > 0) {
-        GoogleTrends.update({'keyword': key}, newData);
-        console.log(`Updated Google Trends for ${key}`, newData);
-      } else {
-        GoogleTrends.create(newData);
-        console.log('created data', newData);
-      }
-    });
-}
+//   GoogleTrends.find({'keyword': key})
+//     .then(function(found) {
+//       if (found.length > 0) {
+//         GoogleTrends.update({'keyword': key}, newData);
+//         console.log(`Updated Google Trends for ${key}`, newData);
+//       } else {
+//         GoogleTrends.create(newData);
+//         console.log('created data', newData);
+//       }
+//     });
+// }
 
 /**
  * function googleTrends.trendData - Updates database for GoogleTrends Summary keywords for dashboard view
  * @param  {[Array]} KEYWORDS [Array of keywords]
  */
 
-googleTrends.trendData(KEYWORDS)
-  .then(function(results) {
+// googleTrends.trendData(KEYWORDS)
+//   .then(function(results) {
 
-    KEYWORDS.forEach((key, index) => {
-      queryGoogleTrends(key, results[index]);
-    });
+//     KEYWORDS.forEach((key, index) => {
+//       queryGoogleTrends(key, results[index]);
+//     });
 
-  })
-  .catch(function(err) {
-    console.error('Error updating Google Trends ', err);
-  });
+//   })
+//   .catch(function(err) {
+//     console.error('Error updating Google Trends ', err);
+//   });
 
 ////////////////////////////////////////////////////////////////////////
 //////Seed database with news and sentiment API
 ////////////////////////////////////////////////////////////////////////
-
-//Set up API keys
-    const alchemy_data_news = watson.alchemy_data_news({
-      api_key: process.env.apikey
-    });
-    const alchemy_language = watson.alchemy_language({
-      api_key: process.env.apikey
-    });
-
-    let paramsNews = {
-      start: 'now-60d',
-      end: 'now',
-      rank: 'high',
-      maxResults: 50,  //change once database is up
-      'q.enriched.url.enrichedTitle.keywords.keyword.text': 'unemployment^inflation^real estate^acquisition^restaurants^dow jones^economy^panic',
-      return: 'enriched.url.title'
-    };
-    let paramsSentiment = {
-      
-      targets: ['inflation','unemployment','real estate', 'acquisition','restaurants','dow jones','economy']
-    };
-
-    const alchemyGetNews = function(params) {
-      return new Promise (function(resolve, reject) {
-        alchemy_data_news.getNews(params, function (err, news) {
-        if (err)
-          reject(err);
-        else
-          resolve(news);
-        });
-      });
-    };
-    const alchemyGetSentiment = function(params) {
-      return new Promise (function(resolve,reject) {
-        alchemy_language.sentiment(params, function(err, sent) {
-          if (err) {
-            reject(err);
-          } else {
-            console.log('sent:',sent)
-            resolve(sent);
-          }
-        });
-      })
-    };
-
-    //Grab data from alchemy news API using target keywords
-    alchemyGetNews(paramsNews)
-      .then(function(news) {
-        let newsArray = news.result.docs;
-        newsArray = newsArray.map(function(val) {
-          return {
-            timestamp: helper.timeConverter(val.timestamp), 
-            title: val.source.enriched.url.title
-          }
-        });
-        newsString = newsArray.reduce(function(prev, cur) {
-          return prev += '. ' + cur.title;
-        }, '');
-        paramsSentiment.text = newsString;
-        //Feed alchemy news data into sentiment API
-        alchemyGetSentiment(paramsSentiment)
-        .then(function(sentiment) {
-          var sentimentArr = sentiment.results.map(function(obj) {
-            return {
-              newsTopic: obj.text,
-              sentimentScore: obj.sentiment.score,
-              type: obj.sentiment.type
-              // relevance: obj.relevance
-            }
-          })
-          console.log('inside worker. sentiment arr:',sentimentArr)
-
-          // for (var i = 0; i < sentimentArr; i++) {
-          //   NSentiment.findOneAndUpdate({newsTopic: sentimentArr.newsTopic}, {sentimentScore: sentimentArr.sentimentScore}, {new: true, upsert: true})
-          // }
-          for (var i = 0; i < sentimentArr; i++) {
-            NSentiment.findOneAndUpdate({newsTopic: sentimentArr[i].newsTopic},
-              { $set: { sentimentScore: sentimentArr[i].sentimentScore }}, 
-              { new: true,
-                upsert:true })
-          }
-        })
-        .catch(function(err) {
-          console.log(err);
-        })
-      })
